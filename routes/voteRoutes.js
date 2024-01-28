fbddfaa07c3c771ab47bde2583e87afed3d018be
Nilesh-chaudhary <nileshchaudhary532@gmail.com>
2024-01-28 11:26:45 +0530

    refactored

--- routes/voteRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const VoteController = require("../controllers/voteController.js");
-
-router.post("/:id/vote", VoteController.voteupdate);
-
-module.exports = router;
